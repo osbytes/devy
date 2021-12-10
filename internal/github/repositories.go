01ed80454ed20e14302db5896a53ad0cb084f744
Martin Downes <mndowne@gmail.com>
2021-12-10 10:58:32 -0600

    Added the URL field to the lastrepoupdate command

--- internal/github/repositories.go
+++ internal/github/repositories.go
@@ -12,12 +12,13 @@ import (
 type LastRepo struct {
 	Name        string
 	Description string
+	URL         string
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 }
 
 func (c LastRepo) String() string {
-	msg := fmt.Sprintf("\nThe last repo updated \n\n **%v** \n \t %v \n Created At: %s \n Updated At: %s", c.Name, c.Description, c.CreatedAt.Format("2006-01-02"), c.UpdatedAt.Format("2006-01-02"))
+	msg := fmt.Sprintf("\nThe last repo updated \n\n **%v** \n \t %v \n %v \n Created At: %s \n Updated At: %s", c.Name, c.Description, c.URL, c.CreatedAt.Format("2006-01-02"), c.UpdatedAt.Format("2006-01-02"))
 	if len(c.Name) == 0 {
 		msg = fmt.Sprintf("Could not find a repository")
 	}
@@ -27,12 +28,9 @@ func (c LastRepo) String() string {
 
 func (g *GithubService) GetLastUpdatedRepoByUsername(ctx context.Context, username string) (*LastRepo, error) {
 
-	options := GetContributionsByUsernameOptions{
-		Username: username,
-	}
-
 	lastRepo := &LastRepo{
 		Name:        "",
+		URL:         "",
 		Description: "",
 		CreatedAt:   time.Now(),
 		UpdatedAt:   time.Now(),
@@ -44,6 +42,7 @@ func (g *GithubService) GetLastUpdatedRepoByUsername(ctx context.Context, userna
 				Nodes []struct {
 					Name        string
 					Description string
+					URL         string
 					CreatedAt   time.Time
 					UpdatedAt   time.Time
 				}
@@ -52,13 +51,14 @@ func (g *GithubService) GetLastUpdatedRepoByUsername(ctx context.Context, userna
 	}
 
 	err := g.githubClient.Query(ctx, &lastRepoQuery, map[string]interface{}{
-		"username": githubv4.String(options.Username),
+		"username": githubv4.String(username),
 	})
 	if err != nil {
 		return nil, errors.Wrap(err, "github client query")
 	}
 	lastRepo.Name = lastRepoQuery.User.Repositories.Nodes[0].Name
 	lastRepo.Description = lastRepoQuery.User.Repositories.Nodes[0].Description
+	lastRepo.URL = lastRepoQuery.User.Repositories.Nodes[0].URL
 	lastRepo.CreatedAt = lastRepoQuery.User.Repositories.Nodes[0].CreatedAt
 	lastRepo.UpdatedAt = lastRepoQuery.User.Repositories.Nodes[0].UpdatedAt
 	return lastRepo, nil
